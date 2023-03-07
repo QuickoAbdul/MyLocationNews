bcf7f379c9515e03fe6e46e6c92fd8bab9fa0cd4
Quicko <quickoabdul@gmail.com>
2023-03-07 16:46:48 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import HomeScren from './screens/HomeScreen';
 
 export default function App() {
   return (
+    <SafeAreaProvider>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomeScren/>
     </View>
+    </SafeAreaProvider>
   );
 }
 
@@ -14,7 +19,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ screens/HomeScreen/index.js
@@ -0,0 +1,84 @@
+import React, {Component} from 'react'
+import { View, Text, Image, ScrollView, Linking } from 'react-native'
+import axios from 'axios'
+import { Card, Title, Paragraph } from 'react-native-paper'
+
+import Header from '../../components/AppBar'
+export default class HomeScreen extends Component
+{
+    state ={
+        articles:[],
+        isLoading: true,
+        errors: null,
+    };
+
+    getArticles()
+    {
+        axios
+            .get(
+                "https://newsapi.org/v2/top-headlines?country=us&apiKey=a2e49247ae074d0e9dbe856ee7d5f682"
+            )
+            .then(response =>
+                response.data.articles.map(article => ({
+                    date : `${article.publishedAt}`,
+                    title : `${article.title}`,
+                    url : `${article.url}`,
+                    description : `${article.description}`,
+                    urlToImage : `${article.urlToImage}`,
+                }))
+            )
+            .then(articles => {
+            this.setState({
+                articles,
+                isLoading: false
+            });     
+        })
+        .catch(error => this.setState({ error, isLoading: false}));
+    }
+
+    componentDidMount()
+    {
+        this.getArticles();
+    }
+
+    render()
+    {
+        const {isLoading, articles} = this.state;
+        return (
+            <View>
+                <Header />
+                <ScrollView>
+                    {!isLoading ? (
+                        articles.map(article => {
+                        const {date, title, url, description, urlToImage} = article;
+                        return (
+                            <Card
+                                key={url}
+                                style={{marginTop:10, borderColor:'black', borderRadius:5, borderBottomWidth:1}}
+                                onPress={()=>{Linking.openURL(`${url}`)}}
+                                >
+                                    <View style={{flexDirection:'row'}}>
+                                        <View style={{justifyContent:'space-around', flex:2/3, margin:10}}>
+                                            <Title>{title}</Title>
+                                        </View>   
+                                        <View style={{flex:1/3, margin:10}}>
+                                            <Image style={{width:120, height:120}} source={{uri: urlToImage}}></Image>
+                                        </View> 
+                                    </View>
+                                    <View style={{margin:10}}>
+                                        <Paragraph>{description}</Paragraph>
+                                        <Text>Published At: {date}</Text>
+                                    </View>
+
+                            </Card>
+                        );
+                    })
+                ):(
+                    <Text style={{justifyContent:'center', alignItems:'center'}}>Chargement....</Text>
+                )
+            }
+                </ScrollView>
+            </View>
+        )
+    }
+}
